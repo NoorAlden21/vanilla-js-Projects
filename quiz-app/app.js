96c672f072227596e76e8f9abfe2b9e9733a5390
NoorAlden <NoorAldenBn3@gmail.com>
2025-09-12 01:42:02 +0300

    finish quiz app

--- quiz-app/app.js
+++ quiz-app/app.js
@@ -29,15 +29,84 @@ const questions = [
 ];
 
 const questionEl = document.getElementById("question");
-const answerBtn = document.getElementById("answer-buttons");
+const answerBtns = document.getElementById("answer-buttons");
 const nextBtn = document.getElementById("next-btn");
 
 let currentQuestionIdx = 0;
 let score = 0;
 
-function startQuiz() {
-  questionEl.textContent = questions[currentQuestionIdx].question;
-  console.log(answerBtn);
+function resetState() {
+  nextBtn.style.display = "none";
+  while (answerBtns.firstChild) {
+    answerBtns.removeChild(answerBtns.firstChild);
+  }
 }
+
+function startQuiz() {
+  currentQuestionIdx = 0;
+  score = 0;
+  nextBtn.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIdx];
+  let questionNo = currentQuestionIdx + 1;
+  questionEl.innerHTML = questionNo + ". " + currentQuestion.question;
+  currentQuestion.answers.forEach((answer) => {
+    const btn = document.createElement("button");
+    btn.innerHTML = answer.text;
+    btn.classList.add("btn");
+    answerBtns.appendChild(btn);
+    if (answer.correct) {
+      btn.dataset.correct = answer.correct;
+    }
+    btn.addEventListener("click", selectAnswer);
+  });
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+
+  Array.from(answerBtns.children).forEach((btn) => {
+    if (btn.dataset.correct === "true") {
+      btn.classList.add("correct");
+    }
+    btn.disabled = true;
+  });
+  nextBtn.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionEl.innerHTML = `You scored ${score} out of ${questions.length}`;
+  nextBtn.innerHTML = "Play Again";
+  nextBtn.style.display = "block";
+}
+
+function handleNextBtn() {
+  currentQuestionIdx++;
+  if (currentQuestionIdx < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextBtn.addEventListener("click", () => {
+  if (currentQuestionIdx < questions.length) {
+    handleNextBtn();
+  } else {
+    startQuiz();
+  }
+});
 
 startQuiz();
